346689ea850a44415a986e75189af24844d08c74
Marcus Brandt <mailtilmb@gmail.com>
2023-02-19 12:45:15 +0100

    some changes

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -371,7 +371,6 @@ def batch_transcribe(
     def decode_with_fallback(segment: torch.Tensor) -> DecodingResult:
         temperatures = [temperature] if isinstance(temperature, (int, float)) else temperature
         decode_result = None
-
         for t in temperatures:
             kwargs = {**decode_options}
             if t > 0:
@@ -381,7 +380,7 @@ def batch_transcribe(
             else:
                 # disable best_of when t == 0
                 kwargs.pop("best_of", None)
-
+            
             options = DecodingOptions(**kwargs, temperature=t)
             decode_result = model.decode(segment, options)
 
